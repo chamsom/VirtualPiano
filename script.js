76c28f499c5da718ff8b6deeac239b13c3da338f
Jae Kim <32496701+chamsom@users.noreply.github.com>
2020-06-03 13:07:39 -0500

    Added html/css/js
    .mp3 files included as well...

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyA") {
+        let audio = new Audio("audiofiles/A.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyW") {
+        let audio = new Audio("audiofiles/bruh.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyS") {
+        let audio = new Audio("audiofiles/S.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyE") {
+        let audio = new Audio("audiofiles/yo.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyD") {
+        let audio = new Audio("audiofiles/D.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyF") {
+        let audio = new Audio("audiofiles/F.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyT") {
+        let audio = new Audio("audiofiles/mceat.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyG") {
+        let audio = new Audio("audiofiles/G.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyY") {
+        let audio = new Audio("audiofiles/mchurt.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyH") {
+        let audio = new Audio("audiofiles/H.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyU") {
+        let audio = new Audio("audiofiles/roblox.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyJ") {
+        let audio = new Audio("audiofiles/J.mp3");
+        audio.play();
+    }
+    else {
+        console.log("Invalid input!");
+    }
+});
+
